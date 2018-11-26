9436a03bb5435670983f3218f1735f9db3fc633f
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-11-26 13:53:03 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,12 +1,17 @@
 def hello_t(array)
   if block_given?
+    i = 0
+
+    while i < array.length
+      yield array[i]
+      i += 1
+    end
+
+    array
     
   end
-  i = 0
-  while i < array.length
-    yield array[i]
-    i += 1
-  end
+  
+  
   array
 end
 
